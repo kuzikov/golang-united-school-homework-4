d589e5f0c790feed3519442589e8db6022b06cff
Adam Kuzikov <kuzikov.dev@protonmail.com>
2022-05-27 16:11:11 +0600

    sum nums from the string.

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,9 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"unicode"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -10,6 +13,8 @@ var (
 	errorEmptyInput = errors.New("input is empty")
 	// Use when the expression has number of operands not equal to two
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+	// Use when encounter restricted character in math exppession.
+	errorRestrictedCharSet = errors.New("math expression contains restricted characters")
 )
 
 // Implement a function that computes the sum of two int numbers written as a string
@@ -22,6 +27,84 @@ var (
 //
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
+// StringSum evaluate simple math expression. This is ugly peace of sheat. Got to be rewrited.
 func StringSum(input string) (output string, err error) {
-	return "", nil
+
+	if err := checkInput(input); err != nil {
+		return "", fmt.Errorf("cannot to evaluate expression: %w", err)
+	}
+
+	input = clear(input)
+
+	op1, op2, sig := ops(input)
+
+	if sig == '+' {
+		return strconv.Itoa(op1 + op2), nil
+	}
+	return strconv.Itoa(op1 - op2), nil
+
+}
+
+func clear(input string) string {
+	cleared := make([]rune, 0, len(input))
+	for _, v := range input {
+		if v != ' ' {
+			cleared = append(cleared, v)
+		}
+	}
+	return string(cleared)
+}
+
+func checkInput(input string) error {
+	for _, v := range input {
+
+		if !unicode.IsDigit(v) || v != '+' && v != '-' {
+			return nil
+		}
+		return errorRestrictedCharSet
+
+	}
+	return nil
+}
+
+func ops(expr string) (op1, op2 int, opcode byte) {
+	// op1,op2 sign
+	sg1, sg2 := 1, 1
+	opcode = ' '
+	if expr[0] == byte('-') {
+		sg1 = -1
+		expr = expr[1:]
+	}
+	// fetch op1
+	for i, ch := range expr {
+		if unicode.IsDigit(ch) {
+			op1 = op1*10 + int(ch-'0')
+			continue
+		}
+
+		switch ch {
+		case '+':
+			opcode = '+'
+		default:
+			opcode = '-'
+		}
+		expr = expr[i+1:]
+		break
+
+	}
+
+	for _, ch := range expr {
+		if unicode.IsDigit(ch) {
+			op2 = op2*10 + int(ch-'0')
+			continue
+		}
+		switch ch {
+		case '-':
+			sg2 = -1
+		default:
+			sg2 = 1
+		}
+	}
+
+	return op1 * sg1, op2 * sg2, opcode
 }
---
+++ string_sum_test.go
@@ -0,0 +1,29 @@
+package string_sum
+
+import (
+	"log"
+	"testing"
+)
+
+func TestStrigSum(t *testing.T) {
+	tests := []struct {
+		expr     string
+		expected string
+	}{
+		{expr: "1+1", expected: "2"},
+		{expr: "1-1", expected: "0"},
+		{expr: "-4-6", expected: "-10"},
+		{expr: "-10--1", expected: "-9"},
+		{expr: "-1000+500", expected: "-500"},
+	}
+
+	for _, test := range tests {
+		res, err := StringSum(test.expr)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		if res != test.expected {
+			t.Errorf("have: %v, want:%v\n", res, test.expected)
+		}
+	}
+}
